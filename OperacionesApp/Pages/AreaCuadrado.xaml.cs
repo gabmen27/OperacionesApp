d1c917f6975d6e788b3552a34dfc7f250bb30690
gabmen27 <gabrieljesus680@gmail.com>
2025-03-09 23:10:17 -0600

    Se creo el backend de AreaCuadrado

--- OperacionesApp/Pages/AreaCuadrado.xaml.cs
+++ OperacionesApp/Pages/AreaCuadrado.xaml.cs
@@ -9,11 +9,30 @@ public partial class AreaCuadrado : ContentPage
 
     private void CalcularAreCuadrado_Clicked(object sender, EventArgs e)
     {
+        if (txtLado.Text is null || txtLado.Text == "")
+        {
+            Alerta("Dato no ingresado o Nulo. Intente de Nuevo");
+            txtLado.Focus();
+        }
+        else {
+            double Lado1, calculo;
 
+            Lado1 = Convert.ToDouble(txtLado.Text);
+            calculo = Lado1 * Lado1;
+
+            txtResultado.Text = calculo.ToString();
+        }
     }
 
     private void LimpiarCalculoCuadrado_Clicked(object sender, EventArgs e)
     {
-
+        txtLado.Text = "";
+        txtResultado.Text = "";
+        txtLado.Focus();
+    }
+    //Imprimiendo el Alerta
+    private void Alerta(string mensaje) {
+        DisplayAlert("Advertensia", mensaje, "Aceptar");
+        
     }
 }
