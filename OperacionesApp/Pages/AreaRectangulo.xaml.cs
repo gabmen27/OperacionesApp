dcad1969d6055dd2a03b51d153cc1341292aad18
gabmen27 <gabrieljesus680@gmail.com>
2025-03-09 23:36:11 -0600

    Creacion del BackEnd de AreaRectangulo. Creacion de las funciones Alerta y se Programaron los Botones.

--- OperacionesApp/Pages/AreaRectangulo.xaml.cs
+++ OperacionesApp/Pages/AreaRectangulo.xaml.cs
@@ -10,10 +10,41 @@ public partial class AreaRectangulo : ContentPage
     private void CalcularAreaRectangulo_Clicked(object sender, EventArgs e)
     {
 
+        if (txtBase.Text is null || txtBase.Text =="") {
+
+            Alerta("Dato Nulo o Vacio, Ingrese de nuevo el dato");
+            txtBase.Focus();
+
+        }
+        else if (txtAltura.Text is null || txtAltura.Text=="") {
+
+            Alerta("Dato Nulo o Vacio, Ingrese de nuevo el dato");
+            txtAltura.Focus();
+        }
+        else
+        {
+            double Base1, Altura1, calculo;
+            Base1 = Convert.ToDouble(txtBase.Text);
+            Altura1 = Convert.ToDouble(txtAltura.Text);
+
+            calculo = Base1 * Altura1;
+
+            txtResultado.Text = calculo.ToString();
+        }
+
     }
 
     private void LimpiarRectangulo_Clicked(object sender, EventArgs e)
     {
+        txtBase.Text = "";
+        txtAltura.Text = "";
+        txtResultado.Text = "";
+        txtBase.Focus();
+    }
+
+    private void Alerta(string mensaje)
+    {
+        DisplayAlert("Advertensia", mensaje, "Aceptar");
 
     }
 }
