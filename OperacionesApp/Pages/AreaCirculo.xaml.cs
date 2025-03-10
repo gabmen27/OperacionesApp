dc256b0bc054e1d55f7ad8e4e77a355931466c7f
gabmen27 <gabrieljesus680@gmail.com>
2025-03-09 21:21:21 -0600

    Agregando la funcion CalcularArea y el boton limpiar en el ContentPAge del circulo

--- OperacionesApp/Pages/AreaCirculo.xaml.cs
+++ OperacionesApp/Pages/AreaCirculo.xaml.cs
@@ -7,7 +7,19 @@ public partial class AreaCirculo : ContentPage
 		InitializeComponent();
 	}
 
+	private double CalcularArea(double radio) {
+		radio = Convert.ToDouble(txtRadio.Text);
+
+		Double calculo = 3.14 * Math.Pow(radio,2);
+
+		return calculo;
+	}
     private void CalcularAreaCirculo_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CirculoLimpiarBtn_Clicked(object sender, EventArgs e)
     {
 
     }
